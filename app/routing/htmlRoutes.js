ecd6779e61490f031ad57af79b32e0d4ee42a105
HH964 <49995995+HH964@users.noreply.github.com>
2019-07-21 22:25:13 -0500

    got routes to work in seperate files using body-parser npm

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,25 +1,11 @@
+const path = require('path');
 
-// module.exports = (app) =>{
-//     app.get("/", function(req, res) {
-//         res.sendFile(path.join(__dirname, "/../public/home.html"));
-//     });
-
-//     app.get("/survey", function(req, res) {
-//         res.sendFile(path.join(__dirname, "/../public/survey.html"));
-//     });
-// }
-    
-
-module.exports = (function() {
-    'use strict';
-    var externalRoutes = require('express').Router();
-
-    externalRoutes.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
-    externalRoutes.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+module.exports = function(app){
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
 
-    return externalRoutes;
-})();
+    app.use(function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+}
--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-var express = require("express");
-    var path = require("path");
-        var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require("express");
+    const bodyParser = require("body-parser");
+const app = express();   
+    const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
-module.exports = (app) =>{
-    require("./app/routing/htmlRoutes")(app);
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
-    });
-
-    require("./app/routing/apiRoutes")(app);
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
-    });
-}
+require("./app/routing/htmlRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
