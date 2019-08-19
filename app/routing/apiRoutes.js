1755f75304738ef12757df716d288429190fb3ba
HH964 <49995995+HH964@users.noreply.github.com>
2019-08-19 08:47:37 -0500

    fixed routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,27 @@
 const friends = require("../data/friends");
 
 module.exports = (app) => {
-    app.get("api/friends",(req, res) => {
+    app.get("/api/friends", (req, res) => {
         res.json(friends);
     });
 
-    app.post("/api/friends",(req,res) => {
+    app.post("/api/friends", (req, res) => {
         var newScore = req.body.scores;
-            var scoreArray = [];
+        // console.log(req.body);
+        var scoreArray = [];
         var bestMatch = 0;
 
-        for(var i=0; i< friends.length; i++){
+        for (var i = 0; i < friends.length; i++) {
             var scoreDifference = 0;
 
-            for(var j=0; j< newScore.length; j++){
+            for (var j = 0; j < newScore.length; j++) {
                 scoreDifference += (Math.abs(parseInt(friends[i].scores[j])));
             }
             scoreArray.push(scoreDifference);
         }
 
-        for(var i=0; i< scoreArray.length; i++){
-            if (scoreArray[i]<= scoreArray[bestMatch]){
+        for (var i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] <= scoreArray[bestMatch]) {
                 bestMatch = i;
             }
         }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
 
-    app.use((req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 }
